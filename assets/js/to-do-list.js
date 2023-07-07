8c52504b317fc795a50ace5c13294deddb82ec62
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-07-07 11:34:10 -0600

    funcionalidad de agregar, editar, eliminar pero sin tener tareas pre-definidas, ahora agrega los li alternando los colores entre blanco y gris

--- assets/js/to-do-list.js
+++ assets/js/to-do-list.js
@@ -1,92 +1,111 @@
-let elements= document.getElementById("contElements");
+let elements = document.getElementById("contElements");
 let input = document.getElementById("inputText");
-        let list= document.getElementById("list");
-        let listNum = 0;
-        let backgroundColor = 0;
+let list = document.getElementById("list");
+let listNum = 0;
+let backColor = 2;
 
-addList=()=>{
-    let inputText = filterList(input.value);
-    let currentList;
-    if(inputText)
-    {
-        let listWhite = ` <li class="list-group-item To-do-list-group-item To-do-list-group-white" id= list${listNum}>
-        <div class="form-check ">
-            <input class="form-check-input To-do-list-check" type="checkbox" id="flexCheckDefault${listNum}">
-            <label class="form-check-label To-do-list-text" id="text${listNum}">
-              ${inputText}
-            </label>
-          </div>
-          <div class="To-do-list-group-buttons">
-            <button type="button" class="btn btn-success To-do-list-button" onclick="editList(${listNum})">
-                <span class="fa-solid fa-pen-to-square To-do-list-button-icon"></span>
-            </button>
-            <button type="button" class="btn btn-danger To-do-list-button" onclick="deleteList(${listNum})">
-                <span class="fa-sharp fa-solid fa-trash To-do-list-button-icon"></span>
-            </button>
-          </div>
-        </li> `;
-        list.innerHTML += listWhite;
-        /* currentList = document.getElementById(`list${listNum}`);
-        if(backgroundColor == 0)
-        {
-            currentList.classList.remove("background-$white");
-            currentList.classList.add("background-$pale-gray")
-            backgroundColor = 1;
-        }
-        else{
-            currentList.classList.remove("background-$pale-gray")
-            currentList.classList.add("background-$white");
-            backgroundColor = 0;
-        } */
+const predefinedTasks = [
+  "Task 1",
+  "Task 2",
+  "Task 3"
+];
 
-        input.value = " ";
-        listNum++;
-        contTask();
+addList = () => {
+  let inputText = filterList(input.value);
+  if (inputText) {
+    let listElement = createListElement(inputText);
+    list.appendChild(listElement);
+    input.value = "";
+    listNum++;
+    contTask();
+    if (backColor === 2) {
+      backColor = 3;
+    } else {
+      backColor = 2;
     }
-}
-
-filterList=(x)=>{
-    if (x) {
-         if (x.length > 1) {
-             return x;
-         }
-         else{
-             alert("please enter a task")
-         }
-    }
-    else{
-         return false;
-    }
-}
-
-editList=(listId)=>{
-    let currentText = document.getElementById(`text${listId}`);
-    let newText = prompt("Wanna Change list?",currentText.innerHTML);
-    if (filterList(newText)) {
-        currentText.innerHTML = newText; 
-    }
-}
-
-deleteList=(listId)=>{
-    let current = document.getElementById(`text${listId}`).innerHTML;
-       let deleteComfirm = confirm(`Are you sure to delete ${current}`);
-    if (deleteComfirm) {
-         let p = document.getElementById("list")
-        let c = document.getElementById(`list${listId}`);
-        p.removeChild(c);
-        contTask();
-    }
-    else{
-        console.log("deleted");
-    }
+  }
 };
 
-contTask=()=>{
-    let taskElements = list.querySelectorAll('li');
-    let numTask = taskElements.length;
+createListElement = (text) => {
+  let listItemClass = backColor % 2 === 0 ? 'To-do-list-group-gray' : 'To-do-list-group-white';
 
-    elements.value = " "
-    elements.textContent = `Todos (${numTask})`;
+  let listElement = document.createElement('li');
+  listElement.setAttribute('class', `list-group-item To-do-list-group-item ${listItemClass}`);
+  listElement.setAttribute('id', `list${listNum}`);
 
-}
+  let listItemContent = `
+    <div class="form-check">
+      <input class="form-check-input To-do-list-check" type="checkbox" id="flexCheckDefault${listNum}">
+      <label class="form-check-label To-do-list-text" id="text${listNum}">
+        ${text}
+      </label>
+    </div>
+    <div class="To-do-list-group-buttons">
+      <button type="button" class="btn btn-success To-do-list-button" onclick="editList(${listNum})">
+        <span class="fa-solid fa-pen-to-square To-do-list-button-icon"></span>
+      </button>
+      <button type="button" class="btn btn-danger To-do-list-button" onclick="deleteList(${listNum})">
+        <span class="fa-sharp fa-solid fa-trash To-do-list-button-icon"></span>
+      </button>
+    </div>
+  `;
+  listElement.innerHTML = listItemContent;
 
+  return listElement;
+};
+
+filterList = (x) => {
+  if (x) {
+    if (x.length > 1) {
+      return x;
+    } else {
+      alert("Please enter a task");
+    }
+  } else {
+    return false;
+  }
+};
+
+editList = (listId) => {
+  let currentText = document.getElementById(`text${listId}`);
+  let newText = prompt("Wanna Change list?", currentText.innerHTML);
+  if (filterList(newText)) {
+    currentText.innerHTML = newText;
+  }
+};
+
+deleteList = (listId) => {
+  let current = document.getElementById(`text${listId}`).innerHTML;
+  let deleteConfirm = confirm(`Are you sure to delete ${current}?`);
+  if (deleteConfirm) {
+    let c = document.getElementById(`list${listId}`);
+    c.parentNode.removeChild(c);
+    contTask();
+  } else {
+    console.log("Deletion cancelled");
+  }
+};
+
+contTask = () => {
+  let taskElements = list.querySelectorAll('li');
+  let numTask = taskElements.length;
+
+  elements.value = " ";
+  elements.textContent = `Todos (${numTask})`;
+};
+
+initializePredefinedTasks = () => {
+  for (let i = 0; i < predefinedTasks.length; i++) {
+    let listElement = createListElement(predefinedTasks[i]);
+    list.appendChild(listElement);
+    listNum++;
+    contTask();
+    if (backColor === 2) {
+      backColor = 3;
+    } else {
+      backColor = 2;
+    }
+  }
+};
+
+initializePredefinedTasks();
