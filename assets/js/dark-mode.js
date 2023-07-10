c3409be1a6b1432de077b1f63dbcdf9cd96d356c
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-07-10 14:59:00 -0600

    modo oscuro

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,38 @@
+const switchMode = document.querySelector(".switch-mode");
+let mode = 1;
+
+switchMode.addEventListener("change", () => {
+  if (mode == 1) 
+  {
+   
+    document.querySelector(".switch-mode-icon").classList.remove("fa-sun");
+
+    document.querySelector(".To-do-list").classList.add("dark-mode");
+    document.querySelector(".To-do-list-container").classList.add("dark-mode-container");
+    document.querySelector(".To-do-list-container-text").classList.add("dark-mode-container-text");
+    document.querySelector(".switch-mode-icon").classList.add("fa-moon");
+    document.querySelector(".To-do-list-group-gray").classList.add("dark-mode-group-gray");
+    document.querySelector(".To-do-list-group-white").classList.add("dark-mode-group-white");
+    document.querySelector(".To-do-list-text").classList.add("dark-mode-text");
+    document.querySelector(".btn-success").classList.add("dark-mode-btn");
+    document.querySelector(".To-do-list-check").classList.add("dark-mode-checkBox");
+
+    mode = 0;
+  }
+  else {
+    document.querySelector(".To-do-list").classList.remove("dark-mode");
+    document.querySelector(".To-do-list").classList.remove("dark-mode");
+    document.querySelector(".To-do-list-container").classList.remove("dark-mode-container");
+    document.querySelector(".To-do-list-container-text").classList.remove("dark-mode-container-text");
+    document.querySelector(".switch-mode-icon").classList.remove("fa-moon");
+    document.querySelector(".To-do-list-group-gray").classList.remove("dark-mode-group-gray");
+    document.querySelector(".To-do-list-group-white").classList.remove("dark-mode-group-white");
+    document.querySelector(".To-do-list-text").classList.remove("dark-mode-text");
+    document.querySelector(".btn-success").classList.remove("dark-mode-btn");
+    document.querySelector(".To-do-list-check").classList.remove("dark-mode-checkBox");
+
+    document.querySelector(".switch-mode-icon").classList.add("fa-sun");
+
+    mode = 1;
+  }
+});
