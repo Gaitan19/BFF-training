047507d3e5dcf4430dfeed1bbf6be8fd8e570ce9
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-07-10 16:07:46 -0600

    modo oscuro

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -11,11 +11,12 @@ switchMode.addEventListener("change", () => {
     document.querySelector(".To-do-list-container").classList.add("dark-mode-container");
     document.querySelector(".To-do-list-container-text").classList.add("dark-mode-container-text");
     document.querySelector(".switch-mode-icon").classList.add("fa-moon");
-    document.querySelector(".To-do-list-group-gray").classList.add("dark-mode-group-gray");
-    document.querySelector(".To-do-list-group-white").classList.add("dark-mode-group-white");
-    document.querySelector(".To-do-list-text").classList.add("dark-mode-text");
-    document.querySelector(".btn-success").classList.add("dark-mode-btn");
-    document.querySelector(".To-do-list-check").classList.add("dark-mode-checkBox");
+
+    document.querySelector(".To-do-list-group-gray").classList.add("dark-mode-group-gray"); /*se repite */
+    document.querySelector(".To-do-list-group-white").classList.add("dark-mode-group-white");/*se repite */
+    document.querySelector(".To-do-list-text").classList.add("dark-mode-text");/*se repite */
+    document.querySelector(".btn-success").classList.add("dark-mode-btn");/*se repite */
+    document.querySelector(".To-do-list-check").classList.add("dark-mode-checkBox");/*se repite */
 
     mode = 0;
   }
@@ -25,10 +26,11 @@ switchMode.addEventListener("change", () => {
     document.querySelector(".To-do-list-container").classList.remove("dark-mode-container");
     document.querySelector(".To-do-list-container-text").classList.remove("dark-mode-container-text");
     document.querySelector(".switch-mode-icon").classList.remove("fa-moon");
-    document.querySelector(".To-do-list-group-gray").classList.remove("dark-mode-group-gray");
-    document.querySelector(".To-do-list-group-white").classList.remove("dark-mode-group-white");
-    document.querySelector(".To-do-list-text").classList.remove("dark-mode-text");
-    document.querySelector(".btn-success").classList.remove("dark-mode-btn");
+    
+    document.querySelector(".To-do-list-group-gray").classList.remove("dark-mode-group-gray");/*se repite */
+    document.querySelector(".To-do-list-group-white").classList.remove("dark-mode-group-white");/*se repite */
+    document.querySelector(".To-do-list-text").classList.remove("dark-mode-text");/*se repite */
+    document.querySelector(".btn-success").classList.remove("dark-mode-btn");/*se repite */
     document.querySelector(".To-do-list-check").classList.remove("dark-mode-checkBox");
 
     document.querySelector(".switch-mode-icon").classList.add("fa-sun");
@@ -36,3 +38,4 @@ switchMode.addEventListener("change", () => {
     mode = 1;
   }
 });
+
